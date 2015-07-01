4e8523586a23881aede995d5118b8e07f675fd10
DresnerRobotics <Andrew@trossenrobotics.com>
2015-07-01 06:23:01 -0700

    modified:   Framework/src/vision/ConnectRegions.cpp 	modified:   Framework/src/vision/ImgProcess.cpp 	modified:   Linux/build/LinuxCamera.cpp 	modified:   Linux/project/rme/main.cpp 	modified:   Linux/project/walk_tuner/main.cpp

--- Linux/project/walk_tuner/main.cpp
+++ Linux/project/walk_tuner/main.cpp
@@ -8,7 +8,7 @@
 #include <signal.h>
 #include <libgen.h>
 #include "cmd_process.h"
-#include "mjpg_streamer.h"
+//#include "mjpg_streamer.h"
 
 #define INI_FILE_PATH       "../../../Data/config.ini"
 
@@ -47,8 +47,6 @@ int main(int argc, char *argv[])
 			ini = new minIni(argv[1]);
 		else
 			ini = new minIni(INI_FILE_PATH);
-    mjpg_streamer* streamer = new mjpg_streamer(0, 0);
-    httpd::ini = ini;
 
     //////////////////// Framework Initialize ////////////////////////////
     if(MotionManager::GetInstance()->Initialize(&cm730) == false)
